21d83d1119ae04c2e9e9e544e34817f325b78d89
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-11 15:42:47 +0000

    Create variables.tf

---
+++ network/variables.tf
@@ -0,0 +1,9 @@
+variable "lambda_s3_bucket" {
+  description = "S3 bucket name for Lambda function code"
+  type        = string
+}
+
+variable "lambda_s3_key" {
+  description = "S3 key for Lambda function code"
+  type        = string
+}
